7f2059a63a4aae92dd961fd4abb851e873de93f3
fenn <fenn@gohan.milesaway>
2008-12-09 01:14:27 -0500

    add some unit tests, change class name to Measurement so i don't go nuts, and some input sanity checks before feeding to units

--- skdb.py
+++ skdb.py
@@ -9,26 +9,17 @@ import os
 #unum looks rather immature, perhaps I will write a wrapper for GNU units instead
 #scientific.Physics.PhysicalQuantities looks ok-ish
 
-extract_number = re.compile("\W+")
+class UnitError(Exception): pass 
+class NaNError(Exception): pass
 
-def check_units(string):
-#  for i in ['++', '--', '//', '**', '&', '#', '@', '!', '<', '>', ',', '=', '_', '..']:
-  for i in ['..']: #units bugs out on '1..2'
-    if string.__contains__(i): raise "UnitError", string + ": contains '" + i + "'"
-  rval = os.popen("units -tv '" + string + "'").read()
-  error = re.search('Unknown|Parse|Error| nan ', rval)
-  if error: raise "UnitError", rval
-  if not rval.__contains__('Definition'):
-      raise "UnitError", "unknown problem with units program: " + rval
-  return rval.rstrip('\n')
-
-class Unit:
-  '''i wonder if i should try to preserve the native units'''
-  def __init__(self, string):
-    check_units(string)
-    e_number = '([+-]?\d*\.?\d*([eE][+-]?\d+)?)'
-    match = re.match(e_number + '?(\D*)', string)
-    if match is None: raise "UnitError", string
+class Measurement:
+  '''try to preserve the original units, and provide a wrapper to the GNU units program'''
+  def __init__(self, string, uncertainty=None):
+    self.check(string=string)
+    self.uncertainty = uncertainty
+    e_number = '([+-]?\d*\.?\d*([eE][+-]?\d+)?)' #engineering notation
+    match = re.match(e_number + '?(\D*)$', string)
+    if match is None: raise UnitError, string
     try: self.number = float(match.group(1))
     except ValueError: self.number = 1.0
     self.unit = match.group(3)
@@ -38,24 +29,49 @@ class Unit:
    
   def __mul__(self, other):
     if isinstance(other, Unit):
-      return Unit(str(self.number*other.number) + self.unit + '*' + other.unit)
+      return Measurement(str(self.number*other.number) + self.unit + '*' + other.unit)
     else:
-      return Unit(str(self.number * other) + self.unit)
+      return Measurement(str(self.number * other) + self.unit)
   __rmul__ = __mul__
 
   def __div__(self, other):
-    if isinstance(other, Unit):
-      return Unit(str(self.number/other.number) + self.unit + '/' + other.unit)
+    if isinstance(other, Measurement):
+      return Measurement(str(self.number/other.number) + self.unit + '/' + other.unit)
     else:
-      return Unit(str(self.number / other) + self.unit)
+      return Measurement(str(self.number / other) + self.unit)
   __rdiv__ = __div__
   
   def to(self, dest):
     conv_factor = os.popen("units -t '" + self.__repr__() + "' '" + dest + "'").read().rstrip('\n')
-    return Unit(conv_factor + dest)
+    return Measurement(conv_factor + dest)
 #    return conv_factor + dest
 
-mm = Unit('1mm')
+  def check(self, string=None):
+    if string is None:
+      string = self.__repr__()
+    rval = os.popen("units -t '" + string + "'").read().rstrip('\n')
+    error = re.search('Unknown|Parse|Error', rval)
+    if error:  
+      raise UnitError, rval
+    nan = re.search(' nan?!o', rval)
+    if nan:
+      raise NaNError, rval
+#    if not rval.__contains__('Definition'):
+#      raise UnitError, "unexpected problem with units program: " + rval
+    for i in ['..']: #units bugs out on '1..2'
+      if string.__contains__(i):
+        print "Typo?"+ string + ": contains '" + i + "'"
+    return rval
+
+  def compatible(self, b):
+    '''check if both expressions boil down to the same base units'''
+    try: Measurement(str(self) + '+' + str(b)).check()
+    except UnitError: return None
+    else: return True
+
+
+
+mm = Measurement('1mm')
 #inch = mm*25.4
 #print yaml.dump(inch)
 
