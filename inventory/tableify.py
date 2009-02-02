00e7d0596d6dbdc1803066c2792f5bbc64252565
fenn <fenn@gohan.milesaway>
2009-02-02 15:13:56 -0500

    actually do stuff

--- inventory/tableify.py
+++ inventory/tableify.py
@@ -2,14 +2,34 @@
 
 import yaml
 
-f = open("comparison.yaml")
+f = open('comparison.yaml')
 a = yaml.load(f)
 
 #find the superset
 categories = []
 labs = [lab for lab in a]
 for lab in labs:
-  categories += lab.keys()
+  categories += lab['inventory'].keys()
 categories = [i for i in set(categories)]
 categories.sort()
-print categories
+
+print '<table valign="top" padding="10px">'
+print '<thead>'
+for lab in labs:
+  print '<td>'
+  print '<h2><a href="' + lab['link'] + '">'
+  print '<img src="' + lab['logo'] + '" alt="' + lab['name'] + '"></a></h2>'
+print '</thead>'
+
+for category in categories:
+  print '<tr>'
+  for lab in labs:
+    inv = lab['inventory']
+    print '<td>'
+    print '<b>' + category + ': </b><br>'
+    if inv.__contains__(category) and inv[category]:
+      for item in inv[category]:
+        print '&nbsp;', item, '<br>'
+    print '</td>'
+  print '</tr>'
+print '</table>'
