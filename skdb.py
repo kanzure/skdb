220ff09cbb0d70266c61a2c072a320100f9f1b6e
fenn <fennetic.net>
2009-05-18 03:17:00 -0500

    tabs should be 4 spaces

--- skdb.py
+++ skdb.py
@@ -17,16 +17,16 @@ def simplify(string):
     rval = os.popen("units -t '" + string + "'").read().rstrip('\n')
     error = re.search('Unknown|Parse|Error', rval)
     if error:  
-      raise UnitError, rval
+        raise UnitError, rval
     nan = re.search(' nan?!o', rval)
     if nan:
-      raise NaNError, rval
+        raise NaNError, rval
 #   -t flag takes care of this
 #    if not rval.__contains__('Definition'):
 #      raise UnitError, "unexpected problem with units program: " + rval
     for i in ['..']: #units bugs out on '1..2'
-      if string.__contains__(i):
-        print "Typo?"+ string + ": contains '" + i + "'"
+        if string.__contains__(i):
+            print "Typo?"+ string + ": contains '" + i + "'"
     return rval
 
 def compatible(a, b):
@@ -36,38 +36,38 @@ def compatible(a, b):
     else: return True
 
 class Measurement:
-  '''try to preserve the original units, and provide a wrapper to the GNU units program'''
-  def __init__(self, string, uncertainty=None):
-    simplify(string) #check if we have a good unit format to begin with. is there a better way to do this?
-    self.uncertainty = uncertainty
-    e_number = '([+-]?\d*\.?\d*([eE][+-]?\d+)?)' #engineering notation
-    #match = re.match(e_number + '?(\D*)$', string) #i dunno wtf i was trying to do here
-    match = re.match(e_number + '?(.*)$', string)
-    if match is None: raise UnitError, string
-    try: self.number = float(match.group(1))
-    except ValueError: self.number = 1.0
-    self.unit = match.group(3)
+    '''try to preserve the original units, and provide a wrapper to the GNU units program'''
+    def __init__(self, string, uncertainty=None):
+        simplify(string) #check if we have a good unit format to begin with. is there a better way to do this?
+        self.uncertainty = uncertainty
+        e_number = '([+-]?\d*\.?\d*([eE][+-]?\d+)?)' #engineering notation
+        #match = re.match(e_number + '?(\D*)$', string) #i dunno wtf i was trying to do here
+        match = re.match(e_number + '?(.*)$', string)
+        if match is None: raise UnitError, string
+        try: self.number = float(match.group(1))
+        except ValueError: self.number = 1.0
+        self.unit = match.group(3)
 
-  def __repr__(self):
-    return str(self.number) + self.unit #ew
+    def __repr__(self):
+        return str(self.number) + self.unit #ew
    
-  def __mul__(self, other):
-    if isinstance(other, Unit):
-      return Measurement(str(self.number*other.number) + self.unit + '*' + other.unit)
-    else:
-      return Measurement(str(self.number * other) + self.unit)
-  __rmul__ = __mul__
+    def __mul__(self, other):
+        if isinstance(other, Unit):
+            return Measurement(str(self.number*other.number) + self.unit + '*' + other.unit)
+        else:
+            return Measurement(str(self.number * other) + self.unit)
+    __rmul__ = __mul__
 
-  def __div__(self, other):
-    if isinstance(other, Measurement):
-      return Measurement(str(self.number/other.number) + self.unit + '/' + other.unit)
-    else:
-      return Measurement(str(self.number / other) + self.unit)
-  __rdiv__ = __div__
-  
-  def to(self, dest):
-    conv_factor = os.popen("units -t '" + self.__repr__() + "' '" + dest + "'").read().rstrip('\n')
-    return Measurement(conv_factor + dest)
+    def __div__(self, other):
+        if isinstance(other, Measurement):
+            return Measurement(str(self.number/other.number) + self.unit + '/' + other.unit)
+        else:
+            return Measurement(str(self.number / other) + self.unit)
+    __rdiv__ = __div__
+
+    def to(self, dest):
+        conv_factor = os.popen("units -t '" + self.__repr__() + "' '" + dest + "'").read().rstrip('\n')
+        return Measurement(conv_factor + dest)
 #    return conv_factor + dest
 #  def simplify(self, string):
 
@@ -75,47 +75,47 @@ class Measurement:
 mm = Measurement('1mm')
 
 class Fastener:
-  '''could be a rivet, could be a bolt. duct tape? superglue? twine? hose clamp?
-  these methods are what actually get called by higher levels of abstraction'''
-  def __init__(self, force, rigidity, safety_factor=7):
-    pass
+    '''could be a rivet, could be a bolt. duct tape? superglue? twine? hose clamp?
+    these methods are what actually get called by higher levels of abstraction'''
+    def __init__(self, force, rigidity, safety_factor=7):
+        pass
 
 
 class Thread:
-  '''examples: ballscrews, pipe threads, bolts - NOT any old helix'''
-  def __init__(self, diameter, pitch, form="UN"):
-    self.diameter, self.pitch, self.form = Measurement(diameter), Measurement(pitch), form
+    '''examples: ballscrews, pipe threads, bolts - NOT any old helix'''
+    def __init__(self, diameter, pitch, form="UN"):
+        self.diameter, self.pitch, self.form = Measurement(diameter), Measurement(pitch), form
     
-  def pitch_diameter(self):
-      assert self.form=="UN" and compatible(self.pitch, 'rev/inch'), "this only works for triangular threads atm"
-      s = Template('($diameter)-0.6495919rev/($pitch)') #machinery's handbook 27ed page 1502
-      string = s.safe_substitute(diameter=self.diameter, pitch=self.pitch)
-      return Measurement(simplify(string)).to('in')
+    def pitch_diameter(self):
+        assert self.form=="UN" and compatible(self.pitch, 'rev/inch'), "this only works for triangular threads atm"
+        s = Template('($diameter)-0.6495919rev/($pitch)') #machinery's handbook 27ed page 1502
+        string = s.safe_substitute(diameter=self.diameter, pitch=self.pitch)
+        return Measurement(simplify(string)).to('in')
   
-  def minor_diameter(self):
-      assert self.form=="UN" and compatible(self.pitch, 'rev/inch'), "this only works for triangular threads atm"
-      s = Template('($diameter)-1.299038rev/($pitch)')  #machinery's handbook 27ed page 1502
-      string = s.safe_substitute(diameter=self.diameter, pitch=self.pitch)
-      return Measurement(simplify(string)).to('in')
+    def minor_diameter(self):
+        assert self.form=="UN" and compatible(self.pitch, 'rev/inch'), "this only works for triangular threads atm"
+        s = Template('($diameter)-1.299038rev/($pitch)')  #machinery's handbook 27ed page 1502
+        string = s.safe_substitute(diameter=self.diameter, pitch=self.pitch)
+        return Measurement(simplify(string)).to('in')
     
-  def clamping_force(self, torque, efficiency=0.1):
-    s = Template('($torque)*($pitch)*$efficiency')
-    string = s.safe_substitute(torque=torque, pitch=self.pitch, efficiency=efficiency) #fill in template keywords
-    simplified = simplify(string) #compute the expression
-    force = Measurement(simplified).to('lbf') #I guess this looks better than kg*m/s^2, but there should be a default units setting somewhere
-    return force
+    def clamping_force(self, torque, efficiency=0.1):
+        s = Template('($torque)*($pitch)*$efficiency')
+        string = s.safe_substitute(torque=torque, pitch=self.pitch, efficiency=efficiency) #fill in template keywords
+        simplified = simplify(string) #compute the expression
+        force = Measurement(simplified).to('lbf') #I guess this looks better than kg*m/s^2, but there should be a default units setting somewhere
+        return force
   
-  def tensile_area(self):
-      assert compatible(self.pitch, 'rev/inch')
-      s = Template('pi/4*(($Dm+$Dp)/2)^2') #machinery's handbook 27ed page 1502 formula 9 "tensile-stress area of screw thread"
-      string = s.safe_substitute(Dm=self.minor_diameter(), Dp=self.pitch_diameter())
-      simplified = simplify(string)
-      return Measurement(simplified).to('in^2')
+    def tensile_area(self):
+        assert compatible(self.pitch, 'rev/inch')
+        s = Template('pi/4*(($Dm+$Dp)/2)^2') #machinery's handbook 27ed page 1502 formula 9 "tensile-stress area of screw thread"
+        string = s.safe_substitute(Dm=self.minor_diameter(), Dp=self.pitch_diameter())
+        simplified = simplify(string)
+        return Measurement(simplified).to('in^2')
   
 
 
 class Screw(Fastener):
-  proof_load = {#grade:load, proof load is defined as load bolt can withstand without permanent set
+    proof_load = {#grade:load, proof load is defined as load bolt can withstand without permanent set
         '1':'33ksi',
         '2':'55ksi',
         '3':'85ksi',
@@ -123,7 +123,7 @@ class Screw(Fastener):
         '7':'105ksi',
         '8':'120ksi',
         }
-  tensile_strength = {#grade:load, tensile strength is defined as load bolt can withstand without breaking
+    tensile_strength = {#grade:load, tensile strength is defined as load bolt can withstand without breaking
         '1':'60ksi',
         '2':'74ksi',
         '3':'110ksi',
@@ -131,24 +131,24 @@ class Screw(Fastener):
         '7':'133ksi',
         '8':'150ksi',
         }
-  def __init__(self, thread, length, grade="2"):
-    '''length is defined as the distance from bottom of the head for all screws but 
-    flat head and set screws which use the top of the head instead'''
-    #thread.__init__()
-    self.thread, self.length, self.grade = thread, length, grade
-    #note these tables vary from source to source; might want to check if it really matters to you
+    def __init__(self, thread, length, grade="2"):
+        '''length is defined as the distance from bottom of the head for all screws but 
+        flat head and set screws which use the top of the head instead'''
+        #thread.__init__()
+        self.thread, self.length, self.grade = thread, length, grade
+        #note these tables vary from source to source; might want to check if it really matters to you
+        
+    def max_force(self):
+        s = Template('$area*$strength')
+        string = s.safe_substitute(area=self.thread.tensile_area(), strength=Screw.proof_load[self.grade])
+        simplified = simplify(string)
+        return Measurement(simplified).to('lbf')
   
-  def max_force(self):
-      s = Template('$area*$strength')
-      string = s.safe_substitute(area=self.thread.tensile_area(), strength=Screw.proof_load[self.grade])
-      simplified = simplify(string)
-      return Measurement(simplified).to('lbf')
-  
-  def breaking_force(self):
-      s = Template('$area*$strength')
-      string = s.safe_substitute(area=self.thread.tensile_area(), strength=Screw.tensile_strength[self.grade])
-      simplified = simplify(string)
-      return Measurement(simplified).to('lbf')
+    def breaking_force(self):
+        s = Template('$area*$strength')
+        string = s.safe_substitute(area=self.thread.tensile_area(), strength=Screw.tensile_strength[self.grade])
+        simplified = simplify(string)
+        return Measurement(simplified).to('lbf')
         
 def main():
     screw = yaml.load(open('screw.yaml'))['screw']
@@ -162,4 +162,4 @@ def main():
     print screw.breaking_force()
 
 if __name__ == "__main__":
-  main()
+    main()
--- tests.py
+++ tests.py
@@ -5,29 +5,29 @@ import unittest
 import skdb
 
 class TestUnits(unittest.TestCase):
-  def test_imaginary(self):
-    self.assertRaises(skdb.NaNError, skdb.Measurement('sqrt(-1)').check)
-    self.assertRaises(skdb.NaNError, skdb.Measurement('sqrt(-1mm^2)').check)
-    self.assertRaises(skdb.NaNError, skdb.Measurement('(-1.5e2mm^3)^(1/3)').check)
+    def test_imaginary(self):
+        self.assertRaises(skdb.NaNError, skdb.Measurement('sqrt(-1)').check)
+        self.assertRaises(skdb.NaNError, skdb.Measurement('sqrt(-1mm^2)').check)
+        self.assertRaises(skdb.NaNError, skdb.Measurement('(-1.5e2mm^3)^(1/3)').check)
       
-  def test_weirdshit(self):
-    badunits = ['foobar', 
-      '..', '...', '1...1',
-      '++', '--', '//', '**', '&', '#', '@', '!', '<', '>', ',', '=', '_',]
-      #'1.2.3', #i guess this is all right after all (1.2 * 0.3)
-    for i in badunits:
-      self.assertRaises(skdb.UnitError, skdb.Measurement(i).check)
-  def test_order_of_operations(self):
-    self.assertEqual(skdb.Measurement('25.4mm').to('inch'), skdb.Measurement('in').to('25.4mm'))
-    self.assertEqual(skdb.Measurement('2mm'), skdb.Measurement('sqrt(4mm^2)'))
-  def test_check(self):
-    self.assertTrue(skdb.Measurement('mm').check())
-    self.assertTrue(skdb.Measurement('in-25.4mm').check())
-    self.assertTrue(skdb.Measurement('25.4mm').check())
-  def test_compatible(self):
-    self.assertTrue(skdb.Measurement('25.4mm').compatible(skdb.Measurement('in')))
-  def test_nonscalar(self):
-    self.assertEqual(skdb.Measurement('tempF(212)'), skdb.Measurement('tempC(100)'))
+    def test_weirdshit(self):
+        badunits = ['foobar', 
+            '..', '...', '1...1',
+            '++', '--', '//', '**', '&', '#', '@', '!', '<', '>', ',', '=', '_',]
+            #'1.2.3', #i guess this is all right after all (1.2 * 0.3)
+        for i in badunits:
+            self.assertRaises(skdb.UnitError, skdb.Measurement(i).check)
+    def test_order_of_operations(self):
+        self.assertEqual(skdb.Measurement('25.4mm').to('inch'), skdb.Measurement('in').to('25.4mm'))
+        self.assertEqual(skdb.Measurement('2mm'), skdb.Measurement('sqrt(4mm^2)'))
+    def test_check(self):
+        self.assertTrue(skdb.Measurement('mm').check())
+        self.assertTrue(skdb.Measurement('in-25.4mm').check())
+        self.assertTrue(skdb.Measurement('25.4mm').check())
+    def test_compatible(self):
+        self.assertTrue(skdb.Measurement('25.4mm').compatible(skdb.Measurement('in')))
+    def test_nonscalar(self):
+        self.assertEqual(skdb.Measurement('tempF(212)'), skdb.Measurement('tempC(100)'))
 
 if __name__ == '__main__':
     unittest.main()
