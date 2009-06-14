31f8f43dfe49f2ed696b08f888338890d5cdf6de
Bryan Bishop <kanzure@pikachu.austin.rr.com>
2009-06-14 00:58:29 -0500

    more commenting

--- extract-state-equations.py
+++ extract-state-equations.py
@@ -3,6 +3,8 @@
 #
 # Extract state equations for a mechanical assembly from a graph.
 #
+# usage: python extract-state-equations.py some-sort-of-input
+#
 # Bryan Bishop (kanzure@gmail.com)
 # http://heybryan.org/
 # 2009-06-14
