2d5518b0eb41e83beb9768951175ace5457b4189
Bryan Bishop <kanzure@pikachu.austin.rr.com>
2009-06-15 15:41:38 -0500

    sattle up, we're going bug hunting

--- find-all-cutsets.py
+++ find-all-cutsets.py
@@ -26,6 +26,9 @@ def find_all_paths(graph, start, end, path=[]):
 # a graph is connected if there is a path to go from any given two vertices (ignoring directions)
 def isGraphConnected(graph):
    pathExists = True
+   #print "isGraphConnected: number of edges = ", len(graph.edges())
+   if len(graph.edges()) == 0: # graph can't be connected with no edges
+      return False
    for node in graph.nodes():
       for onode in graph.nodes():
          paths = find_all_paths(graph, node, onode)
@@ -43,14 +46,16 @@ def isGraphConnected(graph):
 # remove the root (given) branch. if the graph is not divided, repeat. if it is, add that branch to the return set.
 
 def cutset(fromNode, toNode, graph):
+   graph2 = copy.copy(graph)
    rset = [(fromNode, toNode)] # return set (we assume it starts with this edge at least)
-   print "function cutset: graph is ", graph
-   graph.del_edge(fromNode, toNode)
-   print "function cutset (2): graph is ", graph
-   if not isGraphConnected(graph): # graphDivided()
-      for each in graph.edges():
+   print "function cutset: graph is ", graph2
+   print "removing:\n\tfromNode = ", fromNode, "\n\ttoNode = ", toNode, "\n"
+   graph2.del_edge(fromNode, toNode)
+   print "function cutset (2): graph is ", graph2
+   if isGraphConnected(graph2): # graphDivided()
+      for each in graph2.edges():
          print "each var is: ", each
-         rset.extend(cutset(each[0],each[1],copy.copy(graph)))
+         rset.extend(cutset(each[0],each[1],copy.copy(graph2)))
    return rset
 
 # now for some testing.
@@ -67,11 +72,38 @@ class TestCut(unittest.TestCase):
             
       # 1-2-3 cutsets: [[(1,2)], [(2,3)]
       g = pygraph.graph() # still not using a digraph
-      g.add_nodes([1,2,3])
+      g.add_nodes([1,2,3,4,5,6,7,8,9])
       g.add_edge(1,2)
       g.add_edge(2,3)
+      g.add_edge(3,4)
+      g.add_edge(4,5)
+      g.add_edge(5,6)
+      g.add_edge(6,7)
+      g.add_edge(7,8)
+      g.add_edge(8,9)
       print "graph is ", g
-      print cutset(1,2,g)
+      thecutset = cutset(7,8,g)#(1,2,g)
+      print "thecutset = ", thecutset
+      self.assertTrue(len(thecutset)==1)
+   
+      # more complicated graph
+      g = pygraph.graph()
+      g.add_nodes(range(1,8))
+      g.add_edge(1,2)
+      g.add_edge(3,2)
+      g.add_edge(4,2)
+      g.add_edge(3,5)
+      g.add_edge(2,4)
+      g.add_edge(6,4)
+      g.add_edge(4,7)
+      g.add_edge(7,4)
+      g.add_edge(7,5) # er, in the graph, there was no directionality here (at all)
+      g.add_edge(5,7)      
+      cutset1 = cutset(3,2,g) # it wants to delete edge (4,6) (which does not exist) :(
+      print "cutset1 = ", cutset1
+    
+
+
 
       # TODO: try pygraph.digraph(). for checking connectedness it should ignore direction.
       # for path finding, directionality does matter in a digraph.
