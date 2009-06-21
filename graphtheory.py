be405e09c735be8b1257c1dbb1d73413b2067da4
Bryan Bishop <kanzure@pikachu.austin.rr.com>
2009-06-21 11:51:44 -0500

    updated graphtheory.py

--- graphtheory.py
+++ graphtheory.py
@@ -84,7 +84,8 @@ def find_cycle(graph):
 #         cycles = reduceCycles(cycles)
 #   return cycles
 
-def isGraphConnected(graph):
+def isGraphConnected(graph): # deprecated
+   # FIXME: countGraphs(g) is probably more efficient than using isGraphConnected()
    pathExists = True
    #print "isGraphConnected: number of edges = ", len(graph.edges())
    if len(graph.edges()) == 0: # graph can't be connected with no edges
@@ -153,7 +154,86 @@ def cutsets(g, howManyResultingGraphs = 2, howManySets = 0):
         #print "cutsets() exiting after creating \t", createdSets, "\t cutsets.\n"
         return returnlist
 
+def separateGraphs(g):
+        # given a graph g with non-connected components, return each component separately.
+        # there's countGraphs(g) number of graphs to find in g.
+        graphs = []
+        #dict = pygraph.accessibility.connected_components(g)
+        #len(list(frozenset(dict.values())))
+        thedict = pygraph.accessibility.connected_components(g)
+        # there's len(frozenset(thedict.keys())) number of graphs to be returned.
+        for each in list(frozenset(thedict.values())):
+                if type(g) == type(pygraph.graph):
+                        g2 = pygraph.graph()
+                if type(g) == type(pygraph.digraph):
+                        g2 = pygraph.digraph()
+                else:
+                        print "ERROR: unknown graph type.\n"
+                        return
+                for node in thedict.keys():
+                        if thedict[node] == each:
+                                g2.add_node(node)
+                                # also, add the edges corresponding to this node in this component
+                                for edge in g.edges():
+                                        if edge[0] == node or edge[1] == node:
+                                                g2.add_edge(edge[0],edge[1])
+                graphs.append(g2)
+        return graphs
+
+def extractAcrossVariables(node):
+        acrossvariables = []
+        return acrossvariables
+
+def extractThroughVariables(node):
+        throughvariables = []
+        return throughvariables
+
+def equationsFromCutsets(g,cutsets): # KCL (Kirchhoff's Circuit Law)
+        # McPhee's Vertex Postulate: the sum of through variables at any node of a system graph must equal zero when due account is taken of the orientation of edges incident upon that node.
+        # Each fundamental cutset breaks the circuit into two pieces: two supernodes. Write a KCL equation for one supernode in each f-cutset (in terms of node voltages). The KCL equations for the two supernodes formed by an f-cutset will be the same. This yields n-1 equations in n node voltage variables. Set one node voltage to zero volts (ground) and solve.
+        equations = []
+        for cutset in cutsets:
+                # TODO: check whether or not the cutset is an f-cutset for the graph
+                g2 = makeCut(cutset,g)
+                graphs = separateGraphs(g2)
+        return equations
+
+def equationsFromCycles(g,cycles): # KVL (Kirchhoff's Voltage Law)
+        # McPhee's Circuit Postulate: the sum of across variables around any circuit of a graph must equal zero when due account is taken of the direction of edges in the circuit.
+        # each variable in the overall graph is given a unique global name.
+        equations = []
+        for cycle in cycles:
+                allAcrossVariables = []
+                # for each node in a cycle
+                for node in cycle:
+                        # extract the across variables at a node
+                        acrossvars = extractAcrossVariables(node)
+                        # add these to the list for this cycle
+                        allAcrossVariables.expand(acrossvars)
+                sumEquation = "" # start off with nothing.
+                for each in allAcrossVariables:
+                        sumEquation = sumEquation + each + "+ "
+                sumEquation = sumEquation + " 0 = 0"
+                equations.append(sumEquation)
+        return equations
+
+def equationsFromNodes(g):
+        equations = []
+        return equations
+
 # unit tests
+
+class TestMisc(unittest.TestCase):
+        def test_separateGraphs(self):
+                # there should be countGraphs(g) number of graphs returned by separateGraphs(g)
+                g = pygraph.graph()
+                g.add_nodes(range(1,11)
+                for each in range(1,10):
+                        g.add_edge(each,each+1)
+                gcut1 = makeCut([1,2],g)
+                counter = countGraphs(gcut1)
+                graphs = separateGraphs(g)
+                self.assertTrue(counter==len(graphs))
 
 class TestCycle(unittest.TestCase):
         #def test_find_all_paths(self):
