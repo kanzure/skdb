ac2341c33b3d49526b27d28f1aa49dc9acbac4d0
Bryan Bishop <kanzure@pikachu.austin.rr.com>
2009-06-28 05:55:41 -0500

    slightly better (non)random coloring in taxonomy-graph.py

--- taxonomy-graph.py
+++ taxonomy-graph.py
@@ -3,7 +3,9 @@ import yaml
 import graph
 import random
 
-def random_color():
+depthcolors = {} #keeping track of colors per each level
+
+def random_color(depth):
     '''A colorvalue may be  "h,s,v"  (hue,  saturation,  brightness)  floating
     point numbers between 0 and 1, or an X11 color name such as white black
     red green blue yellow magenta cyan or burlywood, or a  "#rrggbb"  (red,
@@ -11,14 +13,21 @@ def random_color():
     hue = random.uniform(0,1)
     saturation = random.uniform(0, 0.3)
     brightness = 0.9
-    return '"%f,%f,%f"' % (hue, saturation, brightness)
+    returnstring = '"%f,%f,%f"' % (hue, saturation, brightness)
+    # set color to previous color at this depth (if exists)
+    if depthcolors.has_key(depth):
+        returnstring = (depthcolors[depth])[0]
+        return returnstring
+    # alt functionality: check if there's a color too similar already at this depth.
+    # similarity means ?
+    return returnstring
 
 linnaeus = yaml.load(open('taxonomy.yaml'))
 
 taxonomy = graph.digraph()
 node_id=0 #we need numerical nodes because some terms show up multiple times, like thermal, mechanical, chemical
 
-def walk(treebeard, color, parent_node):
+def walk(treebeard, color, parent_node, depth):
     global node_id
     try:
         for key in treebeard.keys():
@@ -26,13 +35,18 @@ def walk(treebeard, color, parent_node):
             taxonomy.add_node(node_id, [('label', key), ('shape', 'box'),
                 ('fontsize', '24'), ('color', color), ('style', 'filled')])
             taxonomy.add_edge(parent_node, node_id)
-            walk(treebeard[key], random_color(), node_id)
+            color = random_color(depth)
+            # make sure nothing with this same depth has this same color
+            # or, make sure everything at this same depth has this same color
+            if(depthcolors.has_key(depth)): depthcolors[depth].append(color)
+            else: depthcolors[depth] = [color]
+            walk(treebeard[key], color, node_id, depth+1)
     except AttributeError: #leaf node, need some way to peek at contents
         pass #node_id += 1
 
 taxonomy.add_node(node_id, [('label', 'root')])
 #walk(linnaeus['processes']['shaping']['joining'], 'root', node_id)
-walk(linnaeus, 'yellow', node_id)
+walk(linnaeus, 'yellow', node_id, 0)
 
 #run with: python build-taxonomy-graph.py  |dot -Tpng -o'foo.png
 print graph.readwrite.write_dot_graph(taxonomy, False)
