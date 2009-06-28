6d63f26358a82f261c363aab00af7b725dbbc4f2
Bryan Bishop <kanzure@pikachu.austin.rr.com>
2009-06-27 21:49:08 -0500

    dependency resolver system, basic skeleton

---
+++ dep/dep.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# hierarchical resolver
+
+import graph as pygraph
+
+# node class
+class Dependency:
+    def __init__(self, name="dependency"):
+        self.name = name
+    def 
+
+class Resolver(pygraph.digraph):
+    def __init__(self):
+        
+
