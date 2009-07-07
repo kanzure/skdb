e138d0cdafc7da5594dd04d2903efcfce9a53f1d
Smári McCarthy <spm@cauchy.(none)>
2009-07-07 00:37:21 +0000

    Converging, some bugs in the sitedetails.json template though, ignore for now.

---
+++ inventory/media/javascript/latlonselector.js
@@ -0,0 +1,25 @@
+
+
+function LatLonSelector() {
+	var latitude;
+	var longitude;
+	
+	return {
+		init: function(map, latelement, lonelement) {
+			// Map is a instance of the Map class from Map.js
+			// Latelement and lonelement are JQuery objects referring to <input> boxes or similar.
+		},
+		
+		setLocation: function(lat, lon) {
+			
+		},
+		
+		getLocation: function() {
+			
+		},
+		
+		update: function() {
+			
+		}
+	};
+}
