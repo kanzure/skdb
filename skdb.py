cafb10890f26696a89149976914da27c25327c83
fenn <fennetic.net>
2009-07-10 15:32:35 -0500

    more runtime crap

--- skdb.py
+++ skdb.py
@@ -103,7 +103,8 @@ class Range(FennObject):
             b = eval(b)
         return cls(min(a,b), max(a,b))
 
-
+class Distribution(FennObject):
+    yaml_path = ['typical', 'feasible']
 
 #unum looks rather immature, perhaps I will write a wrapper for GNU units instead
 #scientific.Physics.PhysicalQuantities looks ok-ish        
@@ -265,26 +266,49 @@ class Uncertainty(FennObject, Unit):
 
 
 class RuntimeSwitch(FennObject):
+    '''return different values depending on what parameters have been selected.
+    example: !example
+        temperature: !which teatype, temp
+        parameters:
+            teatype:
+                iced:
+                    flavor: lemon
+                    temp: 280K
+                hot:
+                    flavor: earl grey
+                    temp: 340K
+    >>> example(teatype=iced) #note this doesnt actually work yet
+    >>> example.temperature
+    280K'''
+    
     yaml_tag = '!which'
-    def __init__(self, depends, parameter):
+    def __init__(self, depends, parameter=None): #i dont like 'depends' but cant think of a better word
         self.depends, self.parameter = depends, parameter
     def __repr__(self):
         return 'RuntimeSwitch(%s, %s)' % (self.depends, self.parameter)
     def yaml_repr(self):
         return '%s, %s' % (self.depends, self.parameter)
     def get(self):
-        valid_values = package.parameters[self.depends].keys()
-        if package.runtime[self.depends] in valid_values: 
-            return package.parameters[self.depends][package.runtime[self.depends]][self.parameter] #zoinks
+        if self.parameter:
+            valid_values = package.parameters[self.depends].keys()
+            if package.runtime[self.depends] in valid_values: 
+                return package.parameters[self.depends][package.runtime[self.depends]][self.parameter] #zoinks
+            else: 
+                return None 
         else: 
-            return None  #some function to search for similar values here
+            try: return getattr(package.runtime, self.depends) #uh, this is probably wrong
+            except AttributeError: return package[self.depends] #not an object
+  #some function to search for similar values here
 
     @classmethod
     def from_yaml(cls, loader, node):
         '''see http://pyyaml.org/wiki/PyYAMLDocumentation#Constructorsrepresentersresolvers'''
         data = loader.construct_scalar(node)
-        depends, parameter = [i.strip() for i in data.split(',')]
-        return cls(depends, parameter)
+        try: depends, parameter = [i.strip() for i in data.split(',')]
+        except ValueError: #only one argument
+            depends = data.strip()
+            return cls(depends)
+        return cls(depends, parameter=parameter)
 
 
 class Formula(FennObject, str):
