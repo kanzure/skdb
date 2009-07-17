e049fd201bd450b6eec7d5ca2b4c43cd3a3a97f6
kanzure <kanzure@gmail.com>
2009-07-16 21:07:23 -0500

    added pymates, moved code around

---
+++ pymates/pymates.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#pymates
+
+# the following aren't our responsibility, actually (pythonOCC?)
+#class Circle(yaml.YAMLObject)
+#class Cylinder(yaml.YAMLObject)
+#class InterfaceGeom(yaml.YAMLObject):
+
+class Part(yaml.YAMLObject):
+    pass
+
+class Interface(yaml.YAMLObject):
+    '''"units" should be what is being transmitted through the interface, not about the structure.
+    a screw's head transmits a force (N), but not a pressure (N/m**2) because the m**2 is actually interface geometry'''
+    yaml_tag='!interface'
+    def __init__(self, name, units=None, geometry=None):
+        self.name = name
+        self.units = units
+        self.geometry = geometry # need to get a geometry handler class to get everything looking the same
+        # TODO: coordinates (location) of an interface
