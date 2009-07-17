3b4861bf604cce63f7f959258616c15330b36f5c
kanzure <kanzure@gmail.com>
2009-07-16 21:58:32 -0500

    fixed geom.py

--- pymates/geom.py
+++ pymates/geom.py
@@ -9,13 +9,13 @@ class Circle(primitives.PrimitiveShape):
     yaml_tag = '!circle'
     def __init__(self,radius=1,x=0,y=0,z=0,i=numpy.array([0,0,0]),j=numpy.array([0,0,0]),k=numpy.array([0,0,0])):
         '''xyz are positions. ijk are unit vectors describing the local coordinate frame.'''
-        super(x=x,y=y,z=z,i=i,j=j,k=k)
+        primitives.PrimitiveShape(x=x,y=y,z=z,i=i,j=j,k=k)
         self.radius = radius
 
 class Square(primitives.PrimitiveShape):
     '''generic square. units are meters.'''
     yaml_tag = '!square'
     def __init__(self,width=1,height=1,x=0,y=0,z=0,i=numpy.array([0,0,0]),j=numpy.array([0,0,0]),k=numpy.array([0,0,0])):
-        super(x=x,y=y,z=z,i=i,j=j,k=k)
+        primitives.PrimitiveShape.__init__(self,x=x,y=y,z=z,i=i,j=j,k=k)
         self.width = width
         self.height = height
