5a3bf411f971c09449b915ab869675f2f02be880
Smári McCarthy <spm@cauchy.(none)>
2009-07-17 19:39:28 +0000

    More XMLRPC features; changing JSON views to wrap XMLRPC functions (it's faster and cleaner!). Would be good to write a generic JSON wrapper function, hmm.

--- unittests/xmlrpctest.py
+++ unittests/xmlrpctest.py
@@ -7,8 +7,11 @@ import xmlrpclib
 
 server = xmlrpclib.Server("http://127.0.0.1:8000/xmlrpc/")
 
-for site in server.FabMapGetSites():
+print server.GetCapabilities()
+
+for site in server.GetSites():
 	print "Name: %s\nLocation: %s\nLongitude: %lf\nLatitude: %lf\nWebsite: %s" % (site['name'], site['locname'], site['lon'], site['lat'], site['website'])
-	details = server.FabMapGetSiteDetails(site['id'])
+	details = server.GetSiteDetails(site['id'])
 	print details
 	print "-----------------------"
+
