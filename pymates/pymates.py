4c502fd12b03648aa0bdb1b415670bf765b57b64
kanzure <kanzure@gmail.com>
2009-07-17 15:30:51 -0500

    pleasant way to add interfaces

--- pymates/pymates.py
+++ pymates/pymates.py
@@ -3,6 +3,7 @@
 
 import yaml
 import re
+import time
 import geom
 
 # the following aren't our responsibility, actually (pythonOCC?)
@@ -14,29 +15,40 @@ class Part(yaml.YAMLObject):
     '''used for part mating. argh I hope OCC doesn't already implement this and I just don't know it.'''
     yaml_tag = '!part'
     yaml_pattern = re.compile("(.*)")
-    def __init__(self, name="generic part"):
-        self.name = name
+    def __init__(self, description="description", created=time.localtime(), files=[], interfaces={}):
+        self.description, self.created, self.files, self.interfaces = description, created, files, interfaces
     def __repr__(self):
-        return "Part(name=%s)" % (self.name)
+        return "%s(description=%s, created=%s, files=%s, interfaces=%s)" % (self.__class__.__name__, self.description, self.created, self.files, self.interfaces)
     def yaml_repr(self):
-       return "not implemented: %s, %s" % (self.name, self.__class__.__name__) #yaml dumps tag automatically
-    @classmethod
-    def to_yaml(cls, dumper, data):
-        return dumper.represent_scalar(cls.yaml_tag, cls.yaml_repr(data))
-    @classmethod
-    def from_yaml(cls, loader, node):
-        print "from_yaml() says node = ", node
-        print "from_yaml() says that loader = ", loader
-        data = loader.construct_scalar(node)
-        data = loader.construct_mapping(node)
-        return cls(name=data) #better?
+       return "description: %s\ncreated: %s\nfiles: %s\ninterfaces: %s" % (self.description, self.created, self.files, self.interfaces)
+    def __setstate__(self, attrs):
+        #print "Part.__setstate__ says attrs = ", attrs
+        for (k,v) in attrs.items():
+            #self.__setattr__(each[0],each[1])
+            if type(v) == Interface:
+                v.name = k
+                if hasattr(self, "interfaces"): self.interfaces[k] = v
+                else:
+                    self.interfaces = {}
+                    self.interfaces[k] = v
+                #self.interfaces[k] = v
+            self.__setattr__(k,v)
+    #@classmethod
+    #def to_yaml(cls, dumper, data):
+    #    return dumper.represent_mapping(cls.yaml_tag, cls.yaml_repr(data))
+    #@classmethod
+    #def from_yaml(cls, loader, node):
+    #    print "from_yaml() says that loader = ", loader
+    #    data = loader.construct_mapping(node)
+    #    print "from_yaml() says that data = ", data
+    #    return cls(data)
 
 class Interface(yaml.YAMLObject):
     '''"units" should be what is being transmitted through the interface, not about the structure.
     a screw's head transmits a force (N), but not a pressure (N/m**2) because the m**2 is actually interface geometry'''
     yaml_tag = '!interface'
     yaml_pattern = re.compile('(.*)')
-    def __init__(self, name, units=None, geometry=None):
+    def __init__(self, name="generic interface name", units=None, geometry=None):
         self.name = name
         self.units = units
         self.geometry = geometry # need to get a geometry handler class to get everything looking the same
@@ -44,13 +56,13 @@ class Interface(yaml.YAMLObject):
     def __repr__(self):
         return "Interface(name=%s,units=%s,geometry=%s)" % (self.name, self.units, self.geometry)
     def yaml_repr(self):
-        return "%s\n not implemented: foo" % (self.name)
-    @classmethod
-    def to_yaml(cls, dumper, data):
-        return dumper.represent_scalar(cls.yaml_tag, cls.yaml_repr(data))
-    @classmethod
-    def from_yaml(cls, loader, node):
-        return Interface("node name from from_yaml")
+        return "name: %s\nunits: %s\ngeometry: %s" % (self.name, self.units, self.geometry)
+    #@classmethod
+    #def to_yaml(cls, dumper, data):
+    #    return dumper.represent_scalar(cls.yaml_tag, cls.yaml_repr(data))
+    #@classmethod
+    #def from_yaml(cls, loader, node):
+    #    return Interface("node name from from_yaml")
 
 #for cls in [Part, Interface]:
 #    yaml.add_implicit_resolver(cls.yaml_tag, cls.yaml_pattern)
@@ -67,5 +79,6 @@ def load(foo):
 def dump(foo):
     return yaml.dump(foo, default_flow_style=False)
 
-print "loading the file .. it looks like this: ", load(open("models/blockhole.yaml"))
+print "loading the file .. it looks like this: "
+print load(open("models/blockhole.yaml"))["blockhole"]
 #print "dumping the loaded file looks like this: ", dump(load(content))
