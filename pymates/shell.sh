3535812d1a9ae33e1e41a3d54d5565019adbd076
kanzure <kanzure@gmail.com>
2009-07-20 07:57:48 -0500

    interactive shell script added

---
+++ pymates/shell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "interactive python shell for pythonOCC and skdb/pymates"
+echo ">>> pymates.start()"
+echo ">>> pymates.demo()"
+echo ">>> pymates.mate_parts()"
+ipython -wthread -c "import pymates" -i
