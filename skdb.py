c85a402ecf7eea469369d370cd49bcdc6040af14
fenn <fennetic.net>
2009-07-23 15:55:15 -0500

    gah. forgot to save after deleting. units and yaml in their own files for real now

--- skdb.py
+++ skdb.py
@@ -47,78 +47,12 @@ class Package(yaml.YAMLObject):
         self.contributors = contributors
         self.contents = {}
         #TODO inherit from some pretty container class
-    
-sci = '([+-]?\d*.?\d+([eE][+-]?\d+)?)' #exp group leaves turds.. better way to do regex without parens?
 
-class FennObject(yaml.YAMLObject):
-    '''so i dont repeat generic yaml stuff everywhere'''
-    def __setstate__(self, attrs):
-        for (k,v) in attrs.items():
-            k = re.sub(' ', '_', k) #replace spaces with underscores because "foo.the attr" doesn't work
-            self.__setattr__(k,v)
-    @classmethod
-    def to_yaml(cls, dumper, data):
-        if hasattr(cls, 'yaml_repr'):
-            tmp = cls.yaml_repr(data)
-            return dumper.represent_scalar(cls.yaml_tag, tmp)
-        else: 
-            #i want to return the default yaml dump; but how?       
-            #cls.to_yaml = yaml.YAMLObject.to_yaml
-            #return cls.to_yaml(dumper, data)
-            ##return dumper.represent_mapping(cls.yaml_tag, data)
-            tmp = cls.__repr__(data)
-            return dumper.represent_scalar(cls.yaml_tag, tmp)
 
-    @classmethod
-    def from_yaml(cls, loader, node):
-        '''see http://pyyaml.org/wiki/PyYAMLDocumentation#Constructorsrepresentersresolvers'''
-        data = loader.construct_scalar(node)
-        if hasattr(cls, 'yaml_pattern') and hasattr(cls, 'yaml_parse_args') and cls.yaml_parse_args == True:
-            match = re.search(cls.yaml_pattern, data)
-            if match:
-                return cls(match.groups()) #i guess this will stuff the regex groups into the positional args #TODO unit test
-        else:
-            return cls(data)
 
-class Range(FennObject):
-    yaml_tag = "!range"
-    #expression should look something like: 1e4 m .. 2km
-    yaml_pattern = sci+'\s*(\D?.*)?\s*\.\.\s*'+sci+'\s*(\D?.*)$'
-    def __init__(self, min, max):
-        self.min = min
-        self.max = max
-    def __repr__(self):
-        return "Range(%s, %s)" %(self.min, self.max)
-    def yaml_repr(self):
-        return "%s .. %s" %(self.min, self.max)
-    def __eq__(self, other):
-        if type(other) == type(self):
-            return self.min == other.min and self.max == other.max
-        else: return None
-    @classmethod
-    def from_yaml(cls, loader, node):
-        '''see http://pyyaml.org/wiki/PyYAMLDocumentation#Constructorsrepresentersresolvers'''
-        data = loader.construct_scalar(node)
-        match = re.search(cls.yaml_pattern, data)
-        a, crap, units1, b, crap2, units2 = match.groups() 
-        if units2 != '':
-            if units1 != '':
-                a = Unit(a+units1)
-                b = Unit(b+units2)
-            else:
-                a = Unit(a+units2)
-                b = Unit(b+units2)
-        else: 
-            #double yuck. maybe i should just pass this to units instead?
-            a = eval(a)
-            b = eval(b)
-        return cls(min(a,b), max(a,b))
 
 class Distribution(FennObject):
     yaml_path = ['typical', 'feasible']
-
-
-
 
 class RuntimeSwitch(FennObject):
     '''return different values depending on what parameters have been selected.
