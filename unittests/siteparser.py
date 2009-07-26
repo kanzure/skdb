ac62b95bec29ffac2c6ffb9a29653b3dd1041dc8
Smári McCarthy <spm@cauchy.(none)>
2009-07-26 14:18:34 +0000

    Adding some (broken) tests.

---
+++ unittests/siteparser.py
@@ -0,0 +1,22 @@
+import libxml2
+
+fh = open("sitetest.xml")
+input = libxml2.inputBuffer(fh)
+reader = input.newTextReader("")
+
+sites = {}
+reader.Read()
+
+siteID = ""
+
+while reader.Read():
+	if reader.Name() == "site":
+		siteID = reader.GetAttribute("id")
+		sites[siteID] = {}
+	
+	if reader.Name() in ("datetime", "name", "locname", "latitude", "longitude", "website", "access"):
+		print "found " + reader.Name() + " node"
+		sites[siteID][reader.Name()] = reader.ReadInnerXml()
+		
+print sites
+		
