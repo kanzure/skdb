08c23de6b5f1c9e89c7e04d01546eff6a52db653
kanzure <kanzure@gmail.com>
2009-07-26 22:05:44 -0500

    tab hunting season

--- unittests/xmlrpctest.py
+++ unittests/xmlrpctest.py
@@ -10,8 +10,8 @@ server = xmlrpclib.Server("http://127.0.0.1:8000/xmlrpc/")
 print server.GetCapabilities()
 
 for site in server.GetSites():
-	print "Name: %s\nLocation: %s\nLongitude: %lf\nLatitude: %lf\nWebsite: %s" % (site['name'], site['locname'], site['lon'], site['lat'], site['website'])
-	details = server.GetSiteDetails(site['id'])
-	print details
-	print "-----------------------"
+        print "Name: %s\nLocation: %s\nLongitude: %lf\nLatitude: %lf\nWebsite: %s" % (site['name'], site['locname'], site['lon'], site['lat'], site['website'])
+        details = server.GetSiteDetails(site['id'])
+        print details
+        print "-----------------------"
 
