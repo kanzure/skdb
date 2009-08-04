0415d2ef2ca339ec0231cb35a837f73bd2c09a15
fenn <fennetic.net>
2009-08-04 16:05:38 -0500

    change to orientation vector; removing unused stuff

--- core/interface.py
+++ core/interface.py
@@ -7,10 +7,9 @@ class Interface(yaml.YAMLObject):
     '''"units" should be what is being transmitted through the interface, not about the structure.
     a screw's head transmits a force (N), but not a pressure (N/m**2) because the m**2 is actually interface geometry'''
     yaml_tag = '!interface'
-    def __init__(self, name=None, units=None, geometry=None, point=[0,0,0], x=0, y=0, z=0, part=None, max_connections=1):
-        '''rotate first about x, then about z. give x and z in degrees (__init__ autoconverts to radians)'''
-        self.name, self.units, self.geometry, self.degrees, self.part = name, units, geometry, False, part
-        self.point, self.x, self.y, self.z = point, x, y, z
+    def __init__(self, name=None, units=None, geometry=None, point=[0,0,0], orientation=[0,0,1], rotation=0, part=None, max_connections=1):
+        self.name, self.units, self.geometry, self.part = name, units, geometry, part
+        self.point, self.orientation, self.rotation = point, orientation, rotation
         self.max_connections = max_connections
         self.connected = None
         self.identifier = None
@@ -19,7 +18,10 @@ class Interface(yaml.YAMLObject):
         if self.connected >= self.max_connections: return True
         else: return False
     def compatible(self, other):
-        return True #i guess. this should get overwritten for specific problem domains
+        '''Returns True if other is complementary.This method should probably get overwritten for specific problem domains.'''
+        if isinstance(other, self.complement):
+            return True
+        else: return False
     def options(self, parts):
         '''what can this interface connect to?'''
         parts = set(parts) #yay sets!
@@ -31,24 +33,11 @@ class Interface(yaml.YAMLObject):
                     if not i.is_busy() and not self.is_busy():
                         rval.add(Mate(i, self))
         return rval     
-    def convert(self):
-        '''convert from degrees to radians for rotational information'''
-        if not hasattr(self, "degrees"):
-            self.degrees = True
-        if self.degrees == True:
-            print "self.x = ", self.x
-            self.x = self.x * math.pi / 180
-            print "self.x now = ", self.x
-            print "self.y = ", self.y
-            self.y = self.y * math.pi / 180
-            print "self.y now = ", self.y
-            #self.z = self.z * math.pi / 180
-            self.degrees = False
     def __repr__(self):
         if not self.part == None:
             part_name = self.part.name
-        else: part_name = "None"
-        return "Interface(name=%s,id=%s,part.name=%s,units=%s,geometry=%s,point=%s,x=%s,y=%s,z=%s)" % (self.name, self.identifier, part_name, self.units, self.geometry, self.point, self.x, self.y, self.z)
+        else: part_name = None
+        return "Interface("%s")" % (self.name)
     def yaml_repr(self):
         return "name: %s\nidentifier: %s\nhermaphroditic: %s\nunits: %s\ngeometry: %s\npoint: %s\nx: %s\ny: %s\nz: %s\npart: %s" % (self.name, self.identifier, self.hermaphroditic, self.units, self.geometry, self.point, self.x, self.y, self.z, self.part)
 
