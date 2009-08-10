eee5175613af87ae8314a54b38a36cfaea72ef78
fenn <fennetic.net>
2009-08-10 01:53:57 -0500

    a more entertaining demo... shouldnt pymates do something like this?

--- packages/lego/demo.py
+++ packages/lego/demo.py
@@ -1,8 +1,9 @@
 import skdb, yaml
+from copy import copy
 # OUT: Couldn't import OCC.Utils.DataExchange.STEP: Is pythonOCC installed properly?
 lego = skdb.load_package('lego')
 lego.load_data()
-brick1 = lego.parts[0]
-brick2 = lego.parts[0]
-print yaml.dump(brick1.options([brick2]))
-
+brick1 = copy(lego.parts[0])
+brick2 = copy(lego.parts[0])
+#print yaml.dump(brick1.interfaces[1].options([brick1, brick2]))
+print "brick1's stud is compatible with: ", [x.interface1.name for x in brick1.interfaces[5].options([brick1, brick2])]
