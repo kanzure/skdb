7517052d3078f124c4eee29f82383f10f5c2209a
fenn <fennetic.net>
2009-08-10 00:59:55 -0500

    yaml output looks better because vectors are collated (this is because default_flow_style is not False but yaml_flow_style is False)

--- packages/lego/demo.py
+++ packages/lego/demo.py
@@ -1,7 +1,8 @@
-import skdb
+import skdb, yaml
 # OUT: Couldn't import OCC.Utils.DataExchange.STEP: Is pythonOCC installed properly?
 lego = skdb.load_package('lego')
 lego.load_data()
 brick1 = lego.parts[0]
 brick2 = lego.parts[0]
-print brick1.options([brick2])
+print yaml.dump(brick1.options([brick2]))
+
