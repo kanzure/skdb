1016ab66e6f3eef0f7f37a321fe2bae7f0a5bdd2
kanzure <kanzure@gmail.com>
2009-08-15 01:42:14 -0500

    outline of further tests

--- unittests/test_geom.py
+++ unittests/test_geom.py
@@ -23,29 +23,6 @@ class TestGeom(unittest.TestCase):
     def test_part(self):
         '''ditto'''
         part = self.init_part()
-    def test_translation(self):
-        '''test translation'''
-        y_displacement = 10
-        y_init = 5
-        point = [0,y_init,0]
-        trsf1 = translation(point1=[0,0,0], point2=[0,y_displacement,0])
-        result_point = point_trsf(point, trsf1)
-        if not y_displacement == 0:
-            self.assertFalse(result_point == point)
-        self.assertTrue(result_point == [point[0],point[1]+y_displacement,point[2]])
-        #TODO: test translation(vector)
-    def test_rotation(self):
-        '''test rotation'''
-        #test rotation(rotation_pivot_point, direction, angle)
-        rotation_pivot_point = [0,0,0]
-        direction = [0, 0, 1]
-        angle = math.pi
-        point = [5,0,0]
-        trsf1 = rotation(rotation_pivot_point=rotation_pivot_point, direction=direction, angle=angle)
-        result_point = point_trsf(point, trsf1)
-        for n in range(3):
-            self.assertTrue(result_point[n] - [-5,0,0][n] < Precision().Confusion()) #idk the real answer
-        #TODO: test rotation(gp_Ax1, angle)
     def test_point(self):
         point = Point(1,2,3)
         point = Point([1,2,3])
@@ -73,6 +50,41 @@ class TestGeom(unittest.TestCase):
         self.assertEqual(yaml.load("!vector ['0.0', '0.0', 1e-08]"), geom.Vector(0, 0, 0.00000001))
     def test_dir(self):
         dir = Direction(1,2,3)
+    def test_translation(self):
+        '''test translation'''
+        y_displacement = 10
+        y_init = 5
+        point = [0,y_init,0]
+        trsf1 = translation(point1=[0,0,0], point2=[0,y_displacement,0])
+        result_point = point_trsf(point, trsf1)
+        if not y_displacement == 0:
+            self.assertFalse(result_point == point)
+        self.assertTrue(result_point == [point[0],point[1]+y_displacement,point[2]])
+        #TODO: test translation(vector)
+    def test_rotation(self):
+        '''test rotation'''
+        #test rotation(rotation_pivot_point, direction, angle)
+        rotation_pivot_point = [0,0,0]
+        direction = [0, 0, 1]
+        angle = math.pi
+        point = [5,0,0]
+        trsf1 = rotation(rotation_pivot_point=rotation_pivot_point, direction=direction, angle=angle)
+        result_point = point_trsf(point, trsf1)
+        for n in range(3):
+            self.assertTrue(result_point[n] - [-5,0,0][n] < Precision().Confusion()) #idk the real answer
+        #TODO: test rotation(gp_Ax1, angle)
+    def test_transform(self):
+        '''transforms should not be stored in yaml'''
+        pass
+    def test_stacked_transforms(self):
+        pass
+    def test_rotation_transform(self):
+        '''test Rotation (not rotation())'''
+        pass
+    def test_translation_transform(self):
+        '''test Translation (not translation())'''
+        pass
+    #now some unit tests for part mating
 
 if __name__ == "__main__":
     unittest.main()
