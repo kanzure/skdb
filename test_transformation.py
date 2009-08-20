cbc05548af0649eb6e1d4e6718278e246f7418fb
kanzure <kanzure@gmail.com>
2009-08-19 23:03:46 -0500

    playing around with some tests for transformations, please ignore. gp_Ax2 versus gp_Ax3? etc.

---
+++ test_transformation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+from paths import *
+from OCC.Display.wxSamplesGui import start_display
+#test that a shape can be transformed in the same way that a make_vertex object is transformed
+#test also gp_Pnt.Transform and BRepBuilderAPI_Transform
+#do the points line up? vert2 and obj should be the same.
+#now use SetTransformation
+
+trsf1 = gp_Trsf()
+my_ax3 = gp_Ax3(gp_Ax2(gp_Pnt(0,2.1,0), gp_Dir(-1,1,1)))
+trsf1.SetTransformation(my_ax3)
+
+point2 = Point(0,0,0)
+point2.Transform(trsf1)
+vert2 = make_vertex(point2)
+
+#apply BRepBuilderAPI_Transform to vertex1
+vert1 = make_vertex(Point(0,2.1,0))
+obj = BRepBuilderAPI_Transform(vert1, trsf1, True).Shape()
+
+my_ax = gp_Ax2(gp_Pnt(0,2.1,0), gp_Dir(-1,1,1))
+my_ax.SetDirection(gp_Dir(-1,0,0))
+my_ax = gp_Ax3(my_ax)
+print my_ax.Direct() #when my_ax is gp_Ax3
+blahr = gp_Trsf(); blahr.SetTransformation(my_ax)
+arrow3 = make_arrow_to(dest=blahr) #tests BRepBuilderAPI_Transform
+
+display.DisplayShape(make_vertex(Point(0,0,0))) #display origin marker
+display.DisplayShape(obj) #vert1 but with BRepBuilderAPI_Transform applied to it
+display.DisplayColoredShape(vert2,'GREEN') #make_vertex(point2)
+display.DisplayColoredShape(arrow3,'RED') #make_arrow_to
+
+start_display()
+
