c90868389fe6a05fe81a513d0d898ce5397f8042
kanzure <kanzure@gmail.com>
2009-08-28 22:15:26 -0500

    skdb-get now pulls packages from git repositories

--- clients/skdb-get.py
+++ clients/skdb-get.py
@@ -7,16 +7,19 @@ environmental variables:
 """
 __version__ = "0.0.1"
 
-import sys
+import sys, os
 from skdb import settings
 
 #for parsing command line arguments
 from skdb.core import optfunc
 
-def get_package(package_name, verbose=False, repo=settings.SKDB_PACKAGE_DIR):
+def get_package(package_name, verbose=False, repo=settings.paths["SKDB_PACKAGE_REPO"]):
     '''Usage: %prog <package name> [--verbose] [--repo http://adl.serveftp.org/skdb-packages/]
     download hardware and exit'''
     print "getting package: ", package_name
+    package_url = repo + package_name + ".git"
+    #this is kinda custom for the moment
+    os.system("cd ~/sandbox/; mkdir -p blah; cd blah; git clone %s;" % (package_url))
     return
 
 if __name__ == '__main__':
