db24c9f0ce1a717d464e7736778f2734fdca3eff
fenn <fennetic.net>
2009-09-09 11:44:13 -0500

    trying to clean up geom

--- geom/geom.py
+++ geom/geom.py
@@ -321,31 +321,6 @@ def transformed(self, trsf):
 for i in [load_CAD, add_shape, get_gp_Pnt, transformed]:
     setattr(Part, i.__name__, i)
 
-def test_coordinate_arrows(event=None):
-    for a in 0, 1, -1:
-        for b in 0, 1, -1:
-            for c in 0, 1, -1:
-                try: coordinate_arrow([a, b, c], flag=True)
-                except RuntimeError:
-                    pass
-
-def test_transformation(event=None):
-    brick = get_brick()
-    point = [10,10,10]
-    colors = [ 'WHITE', 'BLUE', 'RED', 'GREEN', 'YELLOW',
-                    'WHITE', 'BLUE', 'RED', 'GREEN', 'YELLOW',
-                    'WHITE', 'BLUE', 'RED', 'GREEN', 'YELLOW']
-    #testfile = '20vert.yaml'
-    #testfile = '60horz.yaml'
-    #testfile = '60twist.yaml'
-    #testfile = '60all.yaml'
-    #testfile = '90vert.yaml'
-    #testfile = '90horz.yaml'
-    testfile = '90twist.yaml'
-    for (i, color) in zip(skdb.load(open(testfile)), colors):
-        trsf = build_trsf(i.point, i.x_vec, i.y_vec)
-        display.DisplayColoredShape(BRepBuilderAPI_Transform(brick._shapes[0], trsf).Shape(), color)
-
 def make_face(shape):
     face = BRepBuilderAPI_MakeFace(shape)
     face.Build()
