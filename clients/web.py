640fa5d9717e4f938c390fafb7a5c1a235d5c219
kanzure <kanzure@gmail.com>
2009-10-16 12:58:37 -0500

    really bad unit tests for cherrypy

--- clients/web.py
+++ clients/web.py
@@ -7,6 +7,7 @@ extra_paths = ["/var/www/", #for skdb on adl.serveftp.org
                "/usr/lib/python2.5/site-packages/Buffet-1.0-py2.5.egg/",
                "/usr/lib/python2.5/site-packages/Cheetah-2.0.1-py2.5-linux-i686.egg/",
                "/usr/lib/python2.5/site-packages/TurboCheetah-1.0-py2.5.egg/",
+               "/home/bryan/code/wsgi/",
                os.path.realpath(os.path.curdir), #for templates
               ]
 def fix_path():
@@ -15,14 +16,24 @@ def fix_path():
         if path not in sys.path: sys.path.append(path)
 fix_path()
 
-#cherrpy
+from copy import copy
+from string import join
+
+#cherrypy
 import atexit
 import threading
 import cherrypy
-from cherrypy import _cperror
+_cperror = cherrypy._cperror
 cherrypy.config.update({'environment': 'embedded'})
 
-#templates
+#cheetah templates
+#         __  ____________  __
+#         \ \/            \/ /
+#          \/    *   *     \/
+#           \      |       / 
+#            \  ==----==  / 
+#             \__________/
+#
 from Cheetah.Template import Template
 from templates import *
 
@@ -37,7 +48,86 @@ def handle_error():
     cherrypy.response.status = 500
     cherrypy.response.body = add_newlines(_cperror.format_exc())
 
-class UnitApp(object):
+class CherryBase(object):
+    def default(self, *extra, **keywords): #TODO
+        if not extra:
+            return self.index(**keywords)
+        url = Url(extra)
+        return_value = """
+        CherryBase.default(extra=%s, keywords=%s)
+        cmd is: %s
+        path is: %s
+        """ % (str(extra), str(keywords), url.cmd, url.path)
+        return add_newlines(return_value)
+    default.exposed=True
+
+class Url:
+    '''a url is parsed into a Url. the format is as follows:
+
+    /home/some/page/new/ -->
+                             parts = ["home", "some", "page", "new"]
+                             cmd = "new"
+                             path = ["home", "some", "page"]
+    /x/y/z/new/1/2/3     -->
+                             parts = ["x", "y", "z", "new", "1", "2", "3"]
+                             cmd = "new"
+                             path = ["x", "y", "z"]
+    
+    in "default" methods, this helps send the user somewhere.
+    useful for a wiki.
+    '''
+    reserved_roots = ["account", "admin"] #for /account stuff.
+    reserved_words = ["new", "delete", "edit", "history", "source", "archive"]
+    def __init__(self, url=str()):
+        self._parts = []
+        self._cmd = ""
+        self._path = []
+        if isinstance(url, Url):
+            self._url = copy(url._url)
+            self._cmd = copy(url._cmd)
+            self._path = copy(url._path)
+        elif isinstance(url, str):
+            self._url = url
+            self.parse(url)
+        elif isinstance(url, tuple):
+            self._path = url
+            self._url = join(url, "/")
+            self.parse(self._url)
+    def parse(self, url):
+        self._url = url
+        if url.count("/") == 0: self._parts = []
+        else:
+            parts = url.split("/")
+            if parts[0] in self.reserved_roots:
+                self._cmd = parts[0]
+            else:
+                path = []
+                for part in parts:
+                    if part in self.reserved_words:
+                        self._cmd = part
+                        self._path = path
+                        break
+                    elif part is not "": path.append(part)
+            self._parts = parts
+    def __str__(self): return self._url
+    def __repr__(self): return str(self)
+    def __eq__(self, other):
+        '''checks if the path is the same (not the parts)
+        so /xyz/new/ matches /xyz/new/123'''
+        if other._path == self._path: return True
+        else: return False
+    def get_path(self):
+        if self._path is not []: return self._path
+        self.parse(self._url)
+        return self._path
+    def get_cmd(self):
+        if self._cmd is not "": return self._cmd
+        self.parse(self._url)
+        return self._cmd
+    cmd=property(fget=get_cmd, doc="returns which command this url corresponds to")
+    path=property(fget=get_path, doc="figures out the path on which the command should operate")
+
+class UnitApp(CherryBase):
     # def __init__(self):
     #     self._tmpl = IndexTemplate()
     def index(self, *extra, **keywords):
@@ -48,23 +138,176 @@ class UnitApp(object):
         else: raise cherrypy.HTTPError(404, "try units/?one=50+m&two=km")
     index.exposed=True
 
-class Root(object):
-    _cp_config = {'request.error_response': handle_error}
-    units = UnitApp() #simple example: /units/?one=m&two=km
+class Uploader(CherryBase):
+    '''simple file upload demo- doesn't really do much right now.'''
+    def index(self):
+        return """
+        <html><body>
+            <form action="upload" method="post" enctype="multipart/form-data">
+            file: <input type="file" name="incoming_file" /><br />
+            <input type="submit" />
+            </form>
+        </body></html>
+        """
+    index.exposed=True
+
+    def upload(self, incoming_file):
+        #get the goods
+        handler = incoming_file.file
+        contents = handler.read()
+        
+        #save the file
+        #TODO: check if file already exists
+        filename = incoming_file.filename
+        target = open("templates/" + filename, "w")
+        target.write(contents)
+        target.close()
+
+        #tell the user
+        return "ok thanks, file has been uploaded"
+    upload.exposed=True
+
+class PackageFront(CherryBase, PackageIndex):
     def __init__(self):
-        self._tmpl = IndexTemplate()
+        PackageIndex.__init__(self)
+    @cherrypy.expose
+    def default(self, *vpath, **keywords):
+        if not vpath:
+            self.index(keywords)
+        #check if vpath refers to a valid package
+        url = Url(vpath)
+        package_name = str(url.path[0])
+        package = skdb.Package(package_name)
+        return str(skdb.yaml.dump(package))
+
+        pass
+    @cherrypy.expose
+    def index(self, **keywords):
+        pass
+
+class App(CherryBase, IndexTemplate):
+    _cp_config = {'request.error_response': handle_error}
+    packages = [] #for keeping skdb packages loaded in memory
+
+    #further apps
+    units = UnitApp() #simple example: /units/?one=m&two=km
+    uploader = Uploader()
+    package = PackageFront()
+
+    def __init__(self):
+        IndexTemplate.__init__(self)
     
     @cherrypy.expose
     def index(self, *extra, **keywords):
-        return self._tmpl.respond() #how do i send locals() to cheetah?
+        return self.respond()
 
-class SiteTest(unittest.TestCase):
-    def test_newliner(self):
-        message="""put text here
-        on to the next line"""
-        result = add_newlines(message)
-        self.assertTrue(result.count("<br />") == 1)
 
-application = cherrypy.Application(Root(), script_name=None, config=None)
+application = cherrypy.Application(App(), script_name=None, config=None)
 if __name__ == "__main__":
-    unittest.main()
+    ##for unittest
+    #tests = [SiteTest]
+    #for test in tests:
+    #    #http://stackoverflow.com/questions/79754/unittest-causing-sys-exit/79833#79833
+    #    unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromTestCase(test))
+
+    #http://projects.dowski.com/files/cp22collection/cp22collection.py?version=colorized
+    from cherrypy.test import helper, webtest
+    webtest.WebCase.interactive = False
+    def setup_server():
+        cherrypy.tree.mount(App(), '/')
+        cherrypy.config.update({
+                'server.log_to_screen': False,
+                'autoreload.on': False,
+                'log_debug_info_filter.on': False,
+                'environment': 'test_suite',
+        })
+
+    class SiteTest(helper.CPWebCase):
+        def test_newliner(self):
+            message="""put text here
+            on to the next line"""
+            result = add_newlines(message)
+            self.assertTrue(result.count("<br />") == 1)
+        def test_url(self):
+            url1 = Url("/home/index/edit")
+            self.assertTrue(url1.cmd == "edit")
+            url2 = Url("/home/index/edit/")
+            self.assertTrue(url2.cmd == "edit")
+            
+            url3 = Url("/home/index/edit/stuff/goes/here")
+            self.assertTrue(url3.cmd == "edit")
+            url4 = Url("/home/index/edit/stuff/goes/")
+            self.assertTrue(url4.cmd == "edit")
+
+            url5 = Url("/home/index/stuff/edit")
+            self.assertTrue(url5.path == ["home", "index", "stuff"])
+            url6 = Url("/path/to/the/file/new/extra/stuff/123")
+            self.assertTrue(url6.path == ["path", "to", "the", "file"])
+        def test_url_eq(self):
+            url1 = Url("/home/index/edit/")
+            url2 = Url("/home/index/edit/blah")
+            self.assertTrue(url1 == url2)
+
+            url3 = Url("/home/index/edit")
+            self.assertTrue(url3 == url2)
+        def test_list_items(self):
+            self.getPage('/widgets/')
+            self.assertBody("All the items")
+
+        def test_creator_page(self):
+            self.getPage('/widgets/creator')
+            self.assertBody("An item creator form")
+
+        def test_create_item(self):
+            self.getPage('/widgets/', method="POST")
+            self.assertBody('Created a new item')
+
+        def test_item_methods(self):
+            expected = {'GET': 'Viewing item 1',
+                        'PUT': 'Updated item 1 with {}',
+                        'DELETE': 'Deleted item 1',
+                        }
+            for meth, response in expected.iteritems():
+                self.getPage('/widgets/1', method=meth)
+                self.assertBody(response)
+
+        def test_item_editor(self):
+            self.getPage('/widgets/22;editor')
+            self.assertBody('An editor form for item 22')
+
+        def test_405s(self):
+            self.getPage('/widgets/10', method='POST')
+            self.assertStatus(405)
+            self.getPage('/widgets/', method='DELETE')
+            self.assertStatus(405)
+
+        def test_404s(self):
+            for path in ['/widgets/view', '/widgets/32;foobar',
+                            '/widgets/33/editor', '/widgets/fluffy',
+                            '/widgets/some/where/out/there']:
+                self.getPage(path)
+                self.assertStatus(404)
+
+        def test_non_mapped(self):
+            """Non-mapped methods should not be available over the web."""
+            self.getPage('/widgets/private')
+            self.assertStatus(404)
+            self.getPage('/widgets/1;private')
+            self.assertStatus(404)
+
+        def test_limited(self):
+            self.getPage('/readonly/')
+            self.assertBody('The list of limited items.')
+
+            #try an unimplemented method
+            self.getPage('/readonly/', method="POST")
+            self.assertStatus(405)
+
+            self.getPage('/readonly/33')
+            self.assertBody('Viewing item 33')
+
+            #try an unimplemented method
+            self.getPage('/readonly/33', method='DELETE')
+            self.assertStatus(405)
+    setup_server()
+    helper.testmain()
