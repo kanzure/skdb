f568de796923991964a8f42bee0da24e629f6764
kanzure <kanzure@gmail.com>
2009-11-29 03:05:46 -0600

    how to install skdb on a fresh copy of ubuntu 9.10

--- web/templates/__init__.py
+++ web/templates/__init__.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python
 from PackageIndex import PackageIndex
 from PackageView import PackageView
 from IndexTemplate import IndexTemplate
-
+from FileViewerTemplate import FileViewerTemplate
 
