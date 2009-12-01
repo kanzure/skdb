c2f93afd3c59d1f5abc42bffcec569dc94c344cb
kanzure <kanzure@gmail.com>
2009-11-30 19:05:31 -0600

    updated skdb-get.py, moved packages to the server.

--- clients/skdb-get.py
+++ clients/skdb-get.py
@@ -2,28 +2,55 @@
 """
 skdb-get.py - "apt-get for real stuff"
 
-environmental variables:
-    - SKDB_PACKAGE_PATH
-"""
-__version__ = "0.0.1"
+run: sudo python skdb-get.py screw
 
+please see ../config.yaml
+"""
 import sys, os
+import getpass
+from string import join
+
+if not getpass.getuser() == "root":
+    print "you must be root"
+    exit(0)
+if not os.path.abspath(__file__).count("clients")>0:
+    print "you must run this from the clients/ directory"
+    exit(0)
+
+#root user probably doesn't know wtf skdb is
+#update $PYTHONPATH to point to skdb
+dir_location = os.path.abspath(__file__)
+print "dir_location is: ", dir_location
+dir_location = dir_location.split("/") #make it a list
+dir_location.pop() #remove "skdb-get.py"
+dir_location.pop() #remove "clients"
+dir_location.pop() #remove skdb folder
+print "dir_location is: ", dir_location
+dir_location = join(dir_location, "/")
+sys.path.append(dir_location) #add it to the python path
+
+import skdb
 from skdb import settings
 
 #for parsing command line arguments
 from skdb.thirdparty import optfunc
 
-def get_package(package_name, verbose=False, repo=settings.paths["SKDB_PACKAGE_REPO"], package_dir=settings.paths["SKDB_PACKAGE_DIR"], **kwarg):
-    '''Usage: %prog <package name> [--verbose] [--repo http://adl.serveftp.org/skdb-packages/] [--package-dir /usr/share/skdb-packages]
+def get_package(package_name, verbose=False, repo=settings.paths["SKDB_PACKAGE_REPO"], package_dir=skdb.settings.path, **keywords):
+    '''usage: %prog <package name> [--verbose] [--repo http://adl.serveftp.org/skdb-packages/] [--package-dir /usr/local/share/skdb/]
     download hardware and exit'''
-    if have_package(package_name):
-        print "package already installed"
+    #assert not have_package(package_name), "package already installed"
+    
     print "getting package: ", package_name
-    package_url = repo + "/" + package_name + ".git"
+    package_url = repo + "/" + package_name + "/.git"
+
+    #make package_dir exist
+    os.system("mkdir -p \"%s\"" % (package_dir))
+
     #this is kinda custom for the moment
-    os.system("cd %s; git clone %s;" % (package_dir, package_url))
+    command = "cd \"%s\"; git clone \"%s\";" % (package_dir, package_url)
+    print "command is: ", command
+    os.system(command)
     return
 
 if __name__ == '__main__':
     optfunc.run(get_package)
-
--- packages/lego/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-from lego import *
-
