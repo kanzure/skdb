8f078193c346a08a37c76373c95be7ac963dfdfc
fenn <fennetic.net>
2010-01-23 13:49:18 -0800

    add trailing slash to url

--- clients/skdb-get.py
+++ clients/skdb-get.py
@@ -22,7 +22,8 @@ def get_package(package_name, verbose=False, repo=settings.paths["repositories"]
     if package_name == "help" or package_name == "": print get_package.__doc__ #how do i do this?
 
     if not isinstance(repo, list): repo = [repo] #it's actually a list in the config file
-    package_url = repo[0] + "/" + package_name + "/.git"
+    if repo[0][-1:] != "/": repo[0] = repo[0]+"/" #add trailing slash to url if not there
+    package_url = repo[0] + package_name + "/.git"
 
     #make package_dir exist
     os.system('mkdir -p "%s"' % (package_dir)) #package names cant have spaces so why the quotes?
@@ -35,7 +36,10 @@ def get_package(package_name, verbose=False, repo=settings.paths["repositories"]
         command = 'cd "%s"; git clone "%s"' % (package_dir, package_url)
         print "installing:", package_name
     if verbose: print "command is: ", command
-    os.system(command)
+    exit_status = os.system(command)
+    if exit_status != 0: raise EnvironmentError, "something went wrong at "+ package_url
+    if verbose: print "checking %s for dependencies..." % (package_name)
+        
     return
 
 if __name__ == '__main__':
